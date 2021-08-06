dab77cafe47b5cfd51992079966071424e25a55e
Till Meyer zu Westram <till.meyerzuwestram@zuehlke.com>
2021-08-06 10:22:28 +0200

    fix database configuration

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
-    "default": {
-        dj_database_url(os.environ.get("DATABASE_URL", f"sqlite:////{BASE_DIR}/db.sqlite3"))
-        #"ENGINE" : "django.db.backends.sqlite3",
-        #"NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    }
+    "default": 
+        dj_database_url.parse(os.environ.get("DATABASE_URL", f"sqlite:////{BASE_DIR}/db.sqlite3"))
 }
 
 # Password validation
